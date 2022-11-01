c193dd1be52909106aca674193c18842dd58fab5
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2022-11-01 13:01:32 -0400

    round one a03

---
+++ bin/cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+//imports
+import minimist from 'minimist';
+
+const args = minimist(process.argv.slice(2));
+
+var sides = args.sides ? args.sides : 6;
+var dice  = args.dice ? args.dice : 2;
+var rolls = args.rolls ? args.rolls : 1;
+
+console.log(JSON.stringify(roll(sides,dice,rolls)));
